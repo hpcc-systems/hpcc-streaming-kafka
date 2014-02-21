443ef238dd0b35ccc22b91ef9e9a3e20cd7d6716
Mike Krumlauf <mjkrumlauf@computer.org>
2014-02-20 20:29:20 -0500

    Modified to host Zookeeper and Kafka broker instances on separate VMs

--- src/main/java/org/hpccsystems/streamapi/service/dao/KafkaMessageDao.java
+++ src/main/java/org/hpccsystems/streamapi/service/dao/KafkaMessageDao.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerTimeoutException;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
@@ -22,8 +24,8 @@ public class KafkaMessageDao implements MessageDao {
     private Producer<String, String> producer;
 
     @Autowired
-    private ConsumerConnector        consumerConnector;
-
+    private ConsumerConfig           consumerConfig;
+    
     @Override
     public void send(final String topic, final List<String> keys, final List<String> messages) {
         final List<KeyedMessage<String, String>> keyedMessages =
@@ -32,46 +34,30 @@ public class KafkaMessageDao implements MessageDao {
         this.producer.send(keyedMessages);
     }
 
-    @Override
-    public List<Message> receive(final String topic) throws HpccStreamingException {
-        final KafkaStream<byte[], byte[]> stream = newKafkaStreamFor(topic);
-
-        final List<Message> messages = obtainMessagesFrom(stream);
-
-        return messages;
-    }
-
     private List<KeyedMessage<String, String>> newKeyedMessagesFrom(
             final String topic, final List<String> keys, final List<String> messages) {
 
         final List<KeyedMessage<String, String>> keyedMessages =
-                new ArrayList<KeyedMessage<String, String>>(keys.size());
+                new ArrayList<>(keys.size());
 
         for (int i = 0; i < keys.size(); i++) {
             final KeyedMessage<String, String> keyedMessage =
-                    new KeyedMessage<String, String>(topic, keys.get(i), messages.get(i));
+                    new KeyedMessage<>(topic, keys.get(i), messages.get(i));
             keyedMessages.add(keyedMessage);
         }
 
         return keyedMessages;
     }
 
-    private KafkaStream<byte[], byte[]> newKafkaStreamFor(final String topic) {
-
-        final Map<String, Integer> streamCounts = Collections.singletonMap(topic, 1);
-
-        final Map<String, List<KafkaStream<byte[], byte[]>>> streams =
-                this.consumerConnector.createMessageStreams(streamCounts);
-
-        final KafkaStream<byte[], byte[]> stream = streams.get(topic).get(0);
-
-        return stream;
-    }
-
-    private List<Message> obtainMessagesFrom(final KafkaStream<byte[], byte[]> stream)
-        throws HpccStreamingException {
-
-        final List<Message> messages = new ArrayList<Message>();
+    @Override
+    public List<Message> receive(final String topic) throws HpccStreamingException {
+        final ConsumerConnector consumerConnector = 
+                Consumer.createJavaConsumerConnector(consumerConfig);
+        
+        final KafkaStream<byte[], byte[]> stream =
+                newKafkaStreamFor(consumerConnector, topic);
+        
+        final List<Message> messages = new ArrayList<>();
         try {
             for (final MessageAndMetadata<byte[], byte[]> messageAndMetadata : stream) {
                 messages.add(new Message(messageAndMetadata));
@@ -79,11 +65,22 @@ public class KafkaMessageDao implements MessageDao {
         } catch (final ConsumerTimeoutException e) {
             throw new HpccStreamingException(e);
         } finally {
-            this.consumerConnector.commitOffsets();
-            this.consumerConnector.shutdown();
+            consumerConnector.commitOffsets();
+            consumerConnector.shutdown();
         }
 
         return messages;
+    }
+
+    private KafkaStream<byte[], byte[]> newKafkaStreamFor(
+            final ConsumerConnector consumerConnector, final String topic) {
+
+        final Map<String, Integer> streamCounts = Collections.singletonMap(topic, 1);
+
+        final Map<String, List<KafkaStream<byte[], byte[]>>> streams =
+                consumerConnector.createMessageStreams(streamCounts);
+
+        return streams.get(topic).get(0);
     }
 
 }
